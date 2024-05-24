6f003565dbd989bcdb997085925472b30d0067fd
Vinicius Emilio <emiliolyra4000@gmail.com>
2024-05-23 21:04:01 -0300

    Criação do Projeto .Java

---
+++ Conta_Bancária/src/Conta_Bancaria.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Conta_Bancaria {
+    public static void main(String[] args) {
+        //Criando objeto Scanner;
+        Scanner scanner = new Scanner(System.in);
+
+        
+    }
+}
